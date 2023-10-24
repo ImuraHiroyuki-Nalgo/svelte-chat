4a7b6fa6c1f0774ed3ffb714f5924223ca26ea69
imura.hiroyuki <imura.hiroyuki@nalgo.co.jp>
2023-10-24 17:50:37 +0900

    ボタンを設けて、クリック時にHello, Worldが出力できることまで確認

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const onClick = () => {
+		console.log('Hello, World!!');
+	};
+</script>
+
+<h1>Firebase Chat</h1>
+<button on:click={onClick}>チャット送信</button>
