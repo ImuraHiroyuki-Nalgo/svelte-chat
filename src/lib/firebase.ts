70da2802aa7d8affde5538d8397ef9c335431a63
imura.hiroyuki <imura.hiroyuki@nalgo.co.jp>
2023-12-19 17:13:24 +0900

    修正

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,11 @@
-import { initializeApp, type FirebaseApp } from "firebase/app";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { Database, getDatabase, ref, type DatabaseReference } from "firebase/database";
+import { getAuth, type Auth } from "firebase/auth";
+import { getDatabase, ref, type Database, type DatabaseReference, push } from "firebase/database";
+import dayjs from "dayjs";
+import { getApps, initializeApp, type FirebaseApp } from 'firebase/app';
+
+import type { Message, Sender } from "$interfaces/message";
+
+import authStore from "$stores/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -9,20 +14,42 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID
+};
+
+export let app: FirebaseApp | null = null;
+export let db: Database | null = null;
+export let dbRef: DatabaseReference | null = null;
+export let auth: Auth | null = null;
+
+export async function initFirebaseApp() {
+  if (getApps().length !== 0) {
+    console.log('Firebase app already initialized.');
+    return;
+  }
+  const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID
+  };
+  app = initializeApp(firebaseConfig);
+  auth = getAuth(app);
+  db = getDatabase(app);
+  dbRef = ref(db, 'chat');
 }
 
-export function isLogined(): Promise<boolean> {
-  return new Promise((resolve) => {
-    onAuthStateChanged(getAuth(), (user) => {
-      if (user) {
-        resolve(true)
-      } else {
-        resolve(false)
-      }
-    })
-  })
-}
+export const sendMessage = async (text: string, sender: Sender) => {
+  if (dbRef === null) {
+    return;
+  }
 
-const app: FirebaseApp = initializeApp(firebaseConfig)
-const db: Database = getDatabase();
-export const dbRef: DatabaseReference = ref(db, 'chat');
+  const message: Message = {
+    text: text,
+    sent_at: dayjs().format(),
+    sender: sender
+  };
+
+  await push(dbRef, message);
+}
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,36 +1,50 @@
 <script lang="ts">
-	import type { ActionData } from './$types';
+	import { goto } from '$app/navigation';
+	import { signInWithEmailAndPassword, getAuth } from 'firebase/auth';
+	import messageStore from '$stores/message';
 
-	export let form: ActionData;
+	let email: string;
+	let password: string;
+
+	const login = async () => {
+		try {
+			const auth = getAuth();
+			await signInWithEmailAndPassword(auth, email, password);
+			await goto('/');
+		} catch (e) {
+			console.error(e);
+			messageStore.set({
+				message:
+					'ログインに失敗しました。入力したアドレス／パスワードが正しいことを確認してください。',
+				type: 'error'
+			});
+		}
+	};
 </script>
 
-<div class="container">
-	<form method="POST">
+<div class="card">
+	<div class="card-content">
 		<div class="row">
 			<div class="input-field col s12">
-				<input name="email" type="email" />
+				<input id="email" type="email" class="validate" bind:value={email} />
 				<label for="email">メールアドレス</label>
 			</div>
 		</div>
 		<div class="row">
 			<div class="input-field col s12">
-				<input name="password" type="password" />
+				<input
+					id="password"
+					type="password"
+					bind:value={password}
+					autocomplete="current-password"
+				/>
 				<label for="password">パスワード</label>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col s12">
-				<button class="btn waves-effect waves-light">ログイン</button>
+				<button class="btn waves-effect waves-light" on:click={login}>ログイン</button>
 			</div>
-		</div>
-	</form>
-	<div class="row">
-		<div class="col s12">
-			{#if form?.isSuccess}
-				ログインに成功しました。
-			{:else if form?.isSuccess === false}
-				<p class="error">ログインに失敗しました。</p>
-			{/if}
 		</div>
 	</div>
 </div>
